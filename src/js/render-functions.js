aa4698c6da4b011bb6c70475bc15d20d824b9a89
Alex Sea <seagullalex81@gmail.com>
2024-07-28 22:36:34 +0300

    add render function

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,2 +1,32 @@
 'use strict';
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+export function creatMarkupImages(images) {
+    const gallery = document.querySelector('.js-gallery');
+    const markup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+        <a href="${largeImageURL}" class="gallery__link">
+            <div class="photo-card">
+                <img src="${webformatURL}" alt="${tags}"/>
+                <div class="info">
+                    <p><strong>Likes:</strong> ${likes}</p>
+                    <p><strong>Views:</strong> ${views}</p>
+                    <p><strong>Comments:</strong> ${comments}</p>
+                    <p><strong>Downloads:</strong> ${downloads}</p>
+                </div>
+            </div>
+        </a>
+    `).join('');
+
+    gallery.insertAdjacentHTML('beforeend', markup);
+
+    const lightbox = new SimpleLightbox('.gallery a');
+    lightbox.refresh();
+}
+
+export function clearGallery() {
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = '';
+}
+
--- src/main.js
+++ src/main.js
@@ -3,8 +3,42 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
 import { searchImagesByQuery } from './js/pixabay-api.js';
+import { creatMarkupImages } from './js/render-functions.js';
+import { clearGallery } from './js/render-functions.js';
 
+const searchForm = document.querySelector('.js-form-container');
+searchForm.addEventListener('submit', searchFoto);
+
+function searchFoto(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const inputValue = form.elements.search.value.toLowerCase().trim();
+
+    if (!inputValue) {
+        iziToast.error({
+            message: 'Please enter the data in the input field',
+            position: 'topRight',
+        });
+        clearGallery();
+        return;
+    }
+
+    searchImagesByQuery(inputValue)
+        .then(data => {
+            if (!data.hits.length) {
+                iziToast.error({
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                    position: 'topRight',
+                });
+                clearGallery();
+                return;
+            }
+            creatMarkupImages(data.hits);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    form.reset();
+}
