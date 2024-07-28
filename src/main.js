600ad3f06cfd4f3bea260714a531ab3a99c16b5d
Alex Sea <seagullalex81@gmail.com>
2024-07-28 11:37:52 +0300

    add function search pixabey

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 'use strict';
 
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+import { searchImagesByQuery } from './js/pixabay-api.js';
+
